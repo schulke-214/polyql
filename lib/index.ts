2a9c94e8bd5f0cc4dba7734749961361ffe6855a
schulke-214 <schulkemaximilian@gmail.com>
2019-10-20 15:16:40 +0200

    fix(client): change wrong usage of the client class

--- lib/index.ts
+++ lib/index.ts
@@ -8,7 +8,7 @@ export * from './types';
 export * from './transporter';
 
 export const fetch = async (url: string, query: string, variables?: Variables): Promise<GraphQLResponse> => {
-	const client: GraphQLClient = new GraphQLClient(url);
+	const client: GraphQLClient = new GraphQLClient({ url });
 
 	return await client.fetch(query, variables);
 };
