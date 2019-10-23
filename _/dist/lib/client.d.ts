7ffe0d4772588816ecd3359d110cdae286e43fc4
schulke-214 <schulkemaximilian@gmail.com>
2019-10-23 22:55:42 +0200

    refactor: remove old code

--- _/dist/lib/client.d.ts
+++
@@ -1,7 +0,0 @@
-import 'cross-fetch/polyfill';
-import { GraphQLResponse, ClientOptions, Variables } from './types';
-export declare class GraphQLClient {
-    private transporter;
-    constructor(options?: ClientOptions);
-    fetch(query: string, variables?: Variables): Promise<GraphQLResponse>;
-}
--- _/dist/lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { GraphQLClient } from './client';
-import { GraphQLResponse, Variables } from './types';
-export default GraphQLClient;
-export { GraphQLClient };
-export * from './types';
-export * from './transporter';
-export declare const fetch: (url: string, query: string, variables?: Variables | undefined) => Promise<GraphQLResponse>;
