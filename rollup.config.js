92bc218896cf4f4055e5275e55a1c89f35e75677
schulke-214 <schulkemaximilian@gmail.com>
2019-10-23 21:34:02 +0200

    build: add typescript support

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,36 @@
+import pkg from './package.json';
+
+import typescript from 'rollup-plugin-typescript';
+import babel from 'rollup-plugin-babel';
 import builtins from 'rollup-plugin-node-builtins';
 import resolve from 'rollup-plugin-node-resolve';
 
 export default {
-	input: 'lib/index.js',
+	input: 'lib/index.ts',
 	output: [
 		{
-			file: 'dist/polyql.es.js',
+			file: pkg.module,
 			format: 'es'
 		},
 		{
-			file: 'dist/polyql.cjs.js',
+			file: pkg.main,
 			format: 'cjs'
+		},
+		{
+			file: pkg.browser,
+			format: 'iife',
+			name: pkg.name
 		}
 	],
-	plugins: [builtins(), resolve()],
+	external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+	plugins: [
+		typescript(),
+		babel({
+			exclude: 'node_modules/**',
+			runtimeHelpers: true
+		}),
+		builtins(),
+		resolve()
+	],
 	external: []
 };
